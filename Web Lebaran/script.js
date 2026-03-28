a0f80da16a271add4e73ce1963aa6c57753e9207
oktaviaramdhani004-ctrl <oktaviaramdhani004@gmail.com>
2026-03-28 21:27:59 +0700

    Delete Web Lebaran/script.js

--- Web Lebaran/script.js
+++
@@ -1,6 +0,0 @@
-function showContent(id) {
-    let sections = document.querySelectorAll('.content-section');
-    sections.forEach(sec => sec.style.display = 'none');
-
-    document.getElementById(id).style.display = 'block';
-}
